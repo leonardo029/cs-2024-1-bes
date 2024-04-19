edb80fa23396f0822cb5e39c1bc913a165090acb
Leonardo Moreira <moreira.leonardo83@outlook.com>
2024-04-19 00:25:42 -0300

    Criação de uma nova classe
    Criação da classe referente ao Bairro

---
+++ endereco/src/main/Bairro.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Bairro {
+    private String nome;
+    private Cidade cidade;
+
+    public Bairro(String nome, Cidade cidade) {
+        this.nome = nome;
+        this.cidade = cidade;
+    }
+}
