4f98821ef87a90d69420be5e439e95e294388b5b
Leonardo Moreira <moreira.leonardo83@outlook.com>
2024-04-19 14:53:38 -0300

    Criação de uma nova classe
    Criação da classe referente ao Logradouro

---
+++ endereco/src/main/Logradouro.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Logradouro {
+    private String nome;
+    private TipoLogradouro tipoLogradouro;
+
+    public Logradouro(String nome, TipoLogradouro tipoLogradouro) {
+        this.nome = nome;
+        this.tipoLogradouro = tipoLogradouro;
+    }
+}
