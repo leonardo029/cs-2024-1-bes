0fd137843a72b46e80d080e58bf1815ef5b45f22
Leonardo Moreira <moreira.leonardo83@outlook.com>
2024-04-22 16:30:20 -0300

    Create Exercicio1.java

---
+++ atividade08/Exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+
+public class Exercicio1 {
+    
+    public static void main(String[] args){
+    
+        System.out.println("Insira o valor do primeiro termo:");
+        System.out.println("Insira o valor do segundo termo:");
+
+        try{
+
+
+
+        }catch(ArithmeticException e){
+        System.out.println("Não é realizar divisões por zero!!");
+        }catch(InputMismatchException e){
+        System.out.println("Insira um valor numérico!!");
+        }
+
+        System.out.println("O resultado da divisão de "++"por"++"é igual a = "+);        
+    }
+
+}
